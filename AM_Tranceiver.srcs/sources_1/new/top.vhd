cad116aa2bcc7d9e434f098230a06d147af83db2
Juan Jimenez <jjimene6@stevens.edu>
2023-03-24 17:18:51 -0400

    testbecnh testing

--- AM_Tranceiver.srcs/sources_1/new/top.vhd
+++ AM_Tranceiver.srcs/sources_1/new/top.vhd
@@ -1,7 +1,11 @@
 LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL, STD.textio.all;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE std.textio.all;
+USE IEEE.std_logic_unsigned.all;
 USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
 
 ENTITY top IS
     PORT (
@@ -23,6 +27,10 @@ signal MCLK : STD_LOGIC;
 signal SCLK : STD_LOGIC;
 signal LRCLK : STD_LOGIC;
 signal adout : STD_LOGIC_VECTOR (23 DOWNTO 0) := "000000000000000000000000";
+signal clk : std_logic := '0';
+signal analog_input : std_logic_vector(15 downto 0) := (others => '0');
+--signal analog_input : STD_LOGIC;
+signal digital_output : std_logic_vector(15 downto 0) := (others => '0');
 file results : text;
 COMPONENT adc_if IS
         PORT (
@@ -55,19 +63,38 @@ adc : adc_if
         data_1 => adout,
         dac_data =>dac_SDIN 
     );
-process(LRCLK)
-variable ILine, OLine : Line;
-variable A_in, B_in, C_in, D_in, Z_out : BIT;
-variable z : bit_vector(23 downto 0);
-variable i : integer := 0;
-variable ch : CHARACTER;
-variable poo : STD_LOGIC_VECTOR(23 downto 0) := "000000000000000000000000";
+
+clk_process : process
 begin
-file_open(results, "C:/Users/17326/AM_Tranceiver/results.txt", WRITE_MODE);
-z := to_bitvector(adout);
-write(OLine, z, right, 25);
-writeline(results, OLine);
-file_close(results);
+   while now < 1 ms loop  -- you can change the simulation time here
+      clk <= not clk;
+      wait for 500 ns;  -- change the clock frequency here
+   end loop;
+   wait;
+end process;
+
+sine_wave : process
+begin
+   for i in 0 to 1000 loop  -- change the simulation time here
+      analog_input <= std_logic_vector(to_unsigned(integer(real(2047.5) * real(sin((real(i)*real(2)*real(3.14)*real(1000))/real(1000000))) + 2047.5), 16));
+      wait for 1 us;  -- change the sample rate here
+   end loop;
+   wait;
+end process;
+
+monitor : process
+variable OLine : Line;
+variable z : bit_vector(23 downto 0);
+begin
+   file_open(results, "C:/Users/17326/AM_Tranceiver/results.txt", WRITE_MODE);
+   while true loop
+      wait until rising_edge(clk);
+      if digital_output /= digital_output'LAST_VALUE then
+         write(OLine, z, right, 25);
+         writeline(results, OLine);
+      end if;
+   end loop;
+   file_close(results);
 end process;
 
 END Behavioral;
